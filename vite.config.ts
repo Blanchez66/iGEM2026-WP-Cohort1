281b92e0f180764af26ab7ab4b05c0ac3d2b53f1
Blanchez66 <1807699931@qq.com>
2026-02-08 20:37:16 +0800

    fix: configure VITE_BASE_PATH for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig, loadEnv, ConfigEnv } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { stringToSlug } from "./src/utils";
 
 // https://vitejs.dev/config/
-export default ({ mode }: ConfigEnv) => {
+export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   const basePath = env.VITE_BASE_PATH || `/${stringToSlug(env.VITE_TEAM_NAME || "igem-starter")}/`;
 
