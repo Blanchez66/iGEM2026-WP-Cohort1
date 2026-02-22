dcac8bf2d1c1ad3c12b859ecf994dc53fdfc77f3
Blanchez66 <1807699931@qq.com>
2026-02-22 21:27:59 +0800

    update:HP

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 import { stringToSlug } from "./src/utils/stringToSlug";
 
 // https://vitejs.dev/config/
@@ -10,5 +11,13 @@ export default ({ mode }) => {
   return defineConfig({
     base: basePath,
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          main: fileURLToPath(new URL("./index.html", import.meta.url)),
+          notFound: fileURLToPath(new URL("./404.html", import.meta.url)),
+        },
+      },
+    },
   });
 };
